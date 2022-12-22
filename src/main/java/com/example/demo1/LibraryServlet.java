397bc8c4f7a51e708cbba1b55a557a1c5decf3eb
monocvet <monocvet@mail.ru>
2022-12-22 19:35:55 +0300

    up

---
+++ src/main/java/com/example/demo1/LibraryServlet.java
@@ -0,0 +1,37 @@
+package com.example.demo1;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet(name = "libraryServlet", value = "/library")
+public class LibraryServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter pw = response.getWriter();
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/my_db",
+                    "postgres", "postgres"
+            );
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from books");
+            while (resultSet.next()) {
+                pw.println(resultSet.getString("title") + " " + resultSet.getString("author")
+                        + " " + resultSet.getString("quantity"));
+            }
+            statement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
